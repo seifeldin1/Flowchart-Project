83182c3e4ed9d333d1cacb76fb490a80d9018131
Dakroury <77912843+seifeldin1@users.noreply.github.com>
2023-12-18 19:02:45 +0200

    Revert "Added all needed files from phase 1 except output"
    This reverts commit 74cbcb5277533e30f8ab40a4d93bb7409c185e3d.

--- GUI/UI_Info.h
+++ GUI/UI_Info.h
@@ -28,8 +28,6 @@ struct UI_Info	//User Interface Info.
 
 	int ASSGN_WDTH;		//Assignment statement default width
 	int ASSGN_HI;		//Assignment statement default height
-	int COND_WDTH;		//conditional statement default width
-	int COND_HI;		//conditional statement default height
 
 }UI;	//create a single global object UI
 
