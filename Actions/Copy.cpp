21ccfa3d817faad9f31d00b99bf282174d2082b7
Dakroury <77912843+seifeldin1@users.noreply.github.com>
2023-12-22 22:36:18 +0200

    copy done

--- Actions/Copy.cpp
+++ Actions/Copy.cpp
@@ -1,1 +1,23 @@
 #include "Copy.h"
+template<typename T>
+Copy<T>::Copy(ApplicationManager* pAppManager) :Action(pAppManager)
+{}
+
+template<typename T>
+void Copy<T>::ReadActionParameters() {
+	Input* pIn = pManager->GetInput();
+	Output* pOut = pManager->GetOutput();
+	pOut->PrintMessage("Select Statment to copy");
+	original=pManager->GetSelectedStatement();
+	
+}
+
+template<typename T>
+void Copy<T>::Execute() {
+	if (original == NULL)return;
+	pManager->AddStatement(original);
+	copy = original;
+	pManager->SetClipboard(copy);
+
+
+}
