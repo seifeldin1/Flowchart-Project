f1ee5d58c541ca2e9876225c15275086774f1d63
Dakroury <77912843+seifeldin1@users.noreply.github.com>
2023-12-26 17:27:02 +0200

    Update HelperFn.cpp

--- HelperFn.cpp
+++ HelperFn.cpp
@@ -69,17 +69,16 @@ bool IsVariable(string input)
 		"alignas", "alignof", "and", "and_eq", "asm", "auto",
 		"bitand", "bitor", "bool", "break", "case", "catch", "char",
 		"char16_t", "char32_t", "class", "compl", "const", "constexpr",
-		"const_cast", "continue", "decltype", "default", "delete", "do",
-		"double", "dynamic_cast", "else", "enum", "explicit", "export",
-		"extern", "false", "float", "for", "friend", "goto", "if", "inline",
-		"int", "long", "mutable", "namespace", "new", "noexcept", "not",
+		"const_cast", "continue", "decltype", "default","int", "long", "mutable", "namespace", "new", "noexcept", "not",
 		"not_eq", "nullptr", "operator", "or", "or_eq", "private", "protected",
 		"public", "register", "reinterpret_cast", "return", "short", "signed",
 		"sizeof", "static", "static_assert", "static_cast", "struct", "switch",
 		"template", "this", "thread_local", "throw", "true", "try", "typedef",
 		"typeid", "typename", "union", "unsigned", "using", "virtual", "void",
-		"volatile", "wchar_t", "while", "xor", "xor_eq"
-		// Add more names if needed
+		"volatile", "wchar_t", "while", "xor", "xor_eq", "delete", "do",
+		"double", "dynamic_cast", "else", "enum", "explicit", "export",
+		"extern", "false", "float", "for", "friend", "goto", "if", "inline"
+		
 	};
 
 	for (int i = 0; i < 84; i++) {
