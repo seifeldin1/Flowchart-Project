4e85e7c21c81592532b852b6dcb54c4ce54dc2fc
Abdelrahman Fathy <abdelrahman.fathy04@outlook.com>
2023-12-27 22:29:19 +0200

    addassign

--- Actions/AddAssign.h
+++ Actions/AddAssign.h
@@ -19,17 +19,18 @@ private:
 
 	Point Position;	//Position where the user clicks to add the stat.
 	//TODO: you should add LHS and RHS of the assignment statement as parameters
+	string LHS;
+	string RHS;
+	int Type;
 
 public:
-	AddAssign(ApplicationManager *pAppManager);
+	AddAssign(ApplicationManager* pAppManager);
 
 	//Read Assignemt statements position
 	virtual void ReadActionParameters();
-	
+
 	//Create and add an assignemnt statement to the list of statements
 	virtual void Execute();
-	int GetOutConnCount() const;
-	
 };
 
 #endif
