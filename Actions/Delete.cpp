8caf3d0e2c16f79277ea86c93639d6642e60d116
Dakroury <77912843+seifeldin1@users.noreply.github.com>
2023-12-27 15:38:14 +0200

    updated

--- Actions/Delete.cpp
+++ Actions/Delete.cpp
@@ -1,1 +1,24 @@
 #include "Delete.h"
+Delete::Delete(ApplicationManager* pAppManager) :Action(pAppManager) {}
+
+void Delete::ReadActionParameters() {
+	Output* pOut = pManager->GetOutput();
+	pOut->PrintMessage("Select a statement or connector to be deleted");
+}
+
+void Delete::Execute() {
+	ReadActionParameters();
+	Statement* delStat = pManager->GetSelectedStatement();
+	Connector* delConn = pManager->GetSelectedConnector();
+	if (!delStat && !delConn) {
+		Output* pOut;
+		pOut->PrintMessage("No selected connectors or statements to delete");
+	}
+
+	else if (delConn) pManager->RemoveConnectorFromList(delConn);
+
+	else if (delStat) {
+		pManager->RemoveStatementFromList(delStat);
+		pManager->DeleteConnectedConnectors(delStat);
+	}
+}
--- Actions/Delete.h
+++ Actions/Delete.h
@@ -1,7 +1,21 @@
 #pragma once
 #include "Action.h"
-class Delete :
-    public Action
+#include "..\Statements\StartStat.h"
+#include "..\Statements\ReadState.h"
+#include "..\Statements\EndStat.h"
+#include "..\Statements\ConditionalState.h"
+#include "..\Statements\AssignStat.h"
+#include "..\Statements\WriteState.h"
+class Delete :public Action
 {
-};
 
+public:
+
+	Delete(ApplicationManager* pManager);
+	//Copy(const Copy& c) : clipboard(c.clipboard);
+
+	void ReadActionParameters();
+	void Execute();
+
+
+};
