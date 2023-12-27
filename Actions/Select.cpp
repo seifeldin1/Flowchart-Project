577252d94873041f8cc09247a4b296339976406d
Omar Mahmoud Demerdash <omds2005@gmail.com>
2023-12-28 00:09:16 +0200

    Selected Almost done

---
+++ Actions/Select.cpp
@@ -0,0 +1,45 @@
+#include "Select.h"
+Select::Select(ApplicationManager* pAppManager) : Action(pAppManager)
+{}
+
+void Select::ReadActionParameters()
+{
+	Input* pIn = pManager->GetInput();
+	Output* pOut = pManager->GetOutput();
+
+	//Read the (Position) parameter
+	pOut->PrintMessage("Select/Unselect Action: Click to select or unselect the statement");
+
+	pIn->GetPointClicked(Position);
+	pOut->ClearStatusBar();
+}
+
+void Select::Execute()
+{
+	ReadActionParameters();
+
+	Statement* StatCheck = pManager->GetStatement(Position);
+	Connector* ConnCheck = pManager->GetConnector(Position);
+	if (StatCheck)
+	{
+		if (StatCheck->IsSelected() == true)
+		{
+			StatCheck->SetSelected(false);
+			pManager->SetSelectedStatement(NULL);
+			return;
+		}
+		else
+		{
+			StatCheck->SetSelected(true);
+			pManager->SetSelectedStatement(StatCheck);
+			(pManager->GetSelectedStatement())->SetSelected(false);
+			return;
+		}
+	}
+	else if (ConnCheck)
+	{
+
+		
+	}
+
+}
