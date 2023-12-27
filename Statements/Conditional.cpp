1c4675da311fadf375853a5479ad0fe2a30fc841
Omar Mahmoud Demerdash Sayed <151223598+omar-demerdash05@users.noreply.github.com>
2023-12-28 00:35:02 +0200

    Delete Statements/Conditional.cpp

--- Statements/Conditional.cpp
+++
@@ -1,5 +0,0 @@
-#include "Conditional.h"
-Statement* Conditional::Copy() {
-	copyConditional = new Conditional(Point(0,0) , ((Conditional*)this)->LHS , ((Conditional*)this)->RHS , ((Conditional*)this)->Comp_operator);
-	return copyConditional;
-}
